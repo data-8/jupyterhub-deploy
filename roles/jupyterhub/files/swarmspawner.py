1676a9016d8d5093f300bc8cece566fde44c2ed7
ryanlovett <rylo@berkeley.edu>
2016-01-21 21:06:40 -0800

    DS8 working dir changes
    Includes:
    - swarmspawner.py fixes
    - assemble_certs fixes
    - get-pip.py
    - rebuild-cull task

--- roles/jupyterhub/files/swarmspawner.py
+++ roles/jupyterhub/files/swarmspawner.py
@@ -29,8 +29,9 @@ class SwarmSpawner(SystemUserSpawner):
     def start(self, image=None, extra_create_kwargs=None, extra_host_config=None):
         # look up mapping of node names to ip addresses
         info = yield self.docker('info')
+        self.log.info("DriverStatus: {}".format(info['DriverStatus']))
         num_nodes = int(info['DriverStatus'][3][1])
-        node_info = info['DriverStatus'][4::8]
+        node_info = info['DriverStatus'][4::5]
         self.node_info = {}
         for i in range(num_nodes):
             node, ip_port = node_info[i]
@@ -56,6 +57,9 @@ class SwarmSpawner(SystemUserSpawner):
 
         # figure out what the node is and then get its ip
         name = yield self.lookup_node_name()
+        s = "DriverStatus: {}".format(info['DriverStatus'])
+        self.log.info(s)
+        #f = open('/tmp/c.log', 'a') ; f.write(s + '\n\n') ; f.close()
         self.user.server.ip = self.node_info[name]
         self.log.info("{} was started on {} ({}:{})".format(
             self.container_name, name, self.user.server.ip, self.user.server.port))
