07214548dfea84da244470a19cdfc69337b9a448
ryanlovett <rylo@berkeley.edu>
2016-08-19 16:19:22 -0700

    Switch to systemuserspawner from swarmspawner.

--- roles/jupyterhub/templates/jupyterhub_config.py
+++ roles/jupyterhub/templates/jupyterhub_config.py
@@ -19,12 +19,15 @@ c.GoogleOAuthenticator.hosted_domain = 'berkeley.edu'
 c.GoogleOAuthenticator.login_service = 'UC Berkeley'
 
 # Configure the spawner
-c.JupyterHub.spawner_class = 'swarmspawner.SwarmSpawner'
+c.JupyterHub.spawner_class = 'systemuserspawner.SystemUserSpawner'
+#c.JupyterHub.spawner_class = 'swarmspawner.SwarmSpawner'
 c.SystemUserSpawner.container_image = 'data8/systemuser'
 c.DockerSpawner.tls_cert = '{{ docker_tls_path }}/cert.pem'
 c.DockerSpawner.tls_key = '{{ docker_tls_path }}/key.pem'
 c.DockerSpawner.remove_containers = True
 c.DockerSpawner.read_only_volumes = {'/home/shared':'/home/shared'}
+c.DockerSpawner.extra_host_config = {'mem_limit': '2g'}
+c.DockerSpawner.container_ip = "0.0.0.0"
 #c.Spawner.start_timeout = 300
 #c.Spawner.http_timeout = 150
 
